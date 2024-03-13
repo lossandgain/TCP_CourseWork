ad82dbe08e9d83499d79357bd11a9f2ef8dca145
lossandgain <105592127+lossandgain@users.noreply.github.com>
2024-03-13 17:32:01 +0300

    Finished Backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,13 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'})
+}
+
 class userController{
     async registration(req, res, next){
         const {email, password, role} = req.body
@@ -13,21 +20,29 @@ class userController{
         if (candidate){
             return next(ApiError.badRequest('Email already exist'))
         }
+
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await User.create({email, role, password: hashPassword})
         const basket = await Basket.create({userId: user.id})
-        const token = jwt.sign({id: user.id, email, role}, process.env.SECRET_KEY, {expiresIn: '24h'})
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
-    async login (req, res){
-
-    }
-    async check(req,res,next){
-        const {id} = req.query
-        if (!id){
-            return next(ApiError.badRequest('Id Error'))
+    async login (req, res, next){
+        const {email, password} = req.body
+        const user = await  User.findOne({where: {email}})
+        if (!user){
+            return next(ApiError.internal('No user found'))
         }
-        res.json(id);
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword){
+            return next(ApiError.internal('Wrong Password'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
+    }
+    async check(req, res, next){
+        const token = generateJwt((req.user.id, req.user.email, req.user.role))
+        return res.json({token})
     }
 }
 
--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -2,7 +2,7 @@ const ApiError = require('../error/ApiError')
 
 module.exports = function (err, req, res, next) {
     if (err instanceof ApiError){
-        return res.stauts(err.status).json({message: err.message})
+        return res.status(err.status).json({message: err.message})
     }
     return res.status(500).json({message: "Unexpected error!"})
 }
